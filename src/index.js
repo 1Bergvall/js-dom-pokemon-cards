4aa59dd806b10ec8f0b864f1b08048919e9e7e9a
Erik Bergvall <erik.bergvall12@gmail.com>
2025-02-04 11:37:28 +0100

    Core and extension complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,84 @@
+function drawCards()
+{
+    data.forEach(pokemon => {
+    let pictureValue = 0;
+    let gameValue = 0;
+    let pictures = [pokemon.sprites.other["official-artwork"].front_default, pokemon.sprites.back_default, pokemon.sprites.front_shiny, pokemon.sprites.back_shiny];
 
-console.log(data);
+    const cards = document.querySelector(".cards");
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+    let el = document.createElement("li");
+    el.className = "card";
+
+
+    let title = document.createElement("h2");
+    title.className = "card--title";
+    title.innerHTML = String(pokemon.name).charAt(0).toUpperCase()+ pokemon.name.slice(1);
+    el.appendChild(title);
+
+
+    let img = document.createElement("img");
+    img.width = "256";
+    img.className = "card--img";
+    img.src =  pictures[0];
+    el.appendChild(img);
+
+    let list = document.createElement("ul");
+    list.className = "card--text";
+
+    pokemon.stats.forEach( stat=> {
+        let item = document.createElement("li");
+        item.innerHTML = stat.stat.name.toUpperCase() + ": " + stat.base_stat;
+        list.appendChild(item);
+    });
+    el.appendChild(list);
+
+    let game = document.createElement("h2");
+    game.innerHTML = "Game Apperances";
+    el.appendChild(game);
+
+    let gameList = document.createElement("ul");
+    gameList.className = "card--text";
+
+    pokemon.game_indices
+
+    let gameItem = document.createElement("li");
+    gameItem.innerHTML = pokemon.game_indices[gameValue].version.name;
+    gameList.appendChild(gameItem);
+    el.appendChild(gameList);
+
+    let previousGame = document.createElement("button");
+    previousGame.className = "previousGame";
+    previousGame.innerHTML = "<";
+    el.appendChild(previousGame);
+
+    let nextGame = document.createElement("button");
+    nextGame.className = "nextGame";
+    nextGame.innerHTML = ">";
+    el.appendChild(nextGame);
+
+    previousGame.addEventListener("click", function(){
+        gameValue = gameValue - 1;
+        if(gameValue < 0)
+            gameValue = 19;
+        gameItem.innerHTML = pokemon.game_indices[gameValue].version.name;
+    });
+    nextGame.addEventListener("click", function(){
+        gameValue = gameValue + 1;
+        if(gameValue > 19)
+            gameValue = 0;
+        gameItem.innerHTML = pokemon.game_indices[gameValue].version.name;
+    });
+    img.addEventListener("click", function(){
+        pictureValue = pictureValue + 1;
+        if(pictureValue > 3)
+            pictureValue = 0;
+        img.src = pictures[pictureValue]
+    });
+    cards.appendChild(el);
+});
+}
+
+drawCards();
+
+
